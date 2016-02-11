bf99f6f21f6be2dd2bce1643b483b0698115ac0b
Thomas Babu <thomas@interuptive.com>
2016-02-11 22:21:48 +0800

    Printing hello world indefinitely

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -4,5 +4,14 @@ package main
 import "fmt"
 
 func main() {
+
+	fmt.Printf("helloworld \n")
+
 	fmt.Println("Helloworld")
+
+	var hello string
+
+	hello = "Hello world"
+
+	fmt.Printf("%s \n", hello)
 }
---
+++ helloworld/hello2.go
@@ -0,0 +1,10 @@
+//Printing hello world indefinitely
+package main
+
+import "fmt"
+
+func main() {
+	for {
+		fmt.Println("Hello World")
+	}
+}
